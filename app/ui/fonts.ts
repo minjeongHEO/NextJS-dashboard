352c71ec80b75b4a63461228208aa68c45f2d7cd
minjeongHEO <hmjoung9770@gmail.com>
2024-11-29 23:04:50 +0900

    feat: 로컬폰트, 구글폰트 적용

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,11 @@
-import { Inter } from 'next/font/google';
+import { Inter, Playfair_Display } from 'next/font/google';
+import localFont from 'next/font/local';
 
 export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Playfair_Display({ subsets: ['latin'], weight: ['400', '900'], display: 'swap' });
+
+export const myFont = localFont({
+  src: './fonts/SF-Pro-Display-Bold.otf',
+  display: 'swap',
+});
