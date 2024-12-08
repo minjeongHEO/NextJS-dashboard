addafae3365d3e3e25f84b81c8b98cb7c49bce7d
minjeongHEO <hmjoung9770@gmail.com>
2024-12-08 09:00:22 +0900

    feat: 페이지 라우팅

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+'use client';
+
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana, myFont } from './ui/fonts';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 export default function Page() {
+  const router = useRouter();
+
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
@@ -28,6 +33,9 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
+        <button type="button" onClick={() => router.push('/dashboard')}>
+          Dashboard
+        </button>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           <Image
             src="/hero-desktop.png"
